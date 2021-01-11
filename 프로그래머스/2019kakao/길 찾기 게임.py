8ccd2f4f10b584237134e239edecc62767adfa86
wnstjr <wnstjr8717@gmail.com>
2021-01-11 23:03:46 +0900

    2021-01-11

---
+++ 프로그래머스/2019kakao/길 찾기 게임.py
@@ -0,0 +1,50 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+
+class Node:
+    def __init__(self, key, position):
+        self.key = key
+        self.position = position
+        self.left = None
+        self.right = None
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+        self.pre = list()
+        self.post = list()
+
+    def insert(self, node):
+        index = 0
+        level = 0
+        for i, j in enumerate(node):
+            if level < j.position[1]:
+                index = i
+                level = j.position[1]
+        self.pre.append(node[index].key) if node else None
+        if 0 <= index < len(node):
+            node[index].left = self.insert(node[:index])
+            node[index].right = self.insert(node[index + 1:])
+        self.root = node[index] if node else None
+        return node[index] if node else None
+
+    def postorder(self, node):
+        if node.left:
+            self.postorder(node.left)
+        if node.right:
+            self.postorder(node.right)
+        self.post.append(node.key)
+
+
+def solution(nodeinfo):
+    node = list(sorted([Node(i + 1, j) for i, j in enumerate(nodeinfo)], key=lambda x: x.position))
+    tree = Tree()
+    tree.insert(node)
+    tree.postorder(tree.root)
+    return [tree.pre, tree.post]
+
+
+ni = [[5, 3], [11, 5], [13, 3], [3, 5], [6, 1], [1, 3], [8, 6], [7, 2], [2, 2]]
+print(solution(ni))
