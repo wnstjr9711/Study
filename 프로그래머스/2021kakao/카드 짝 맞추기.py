4e45ef98f55094b1c09626e9bc5a4e0d6a3d9ad2
wnstjr <wnstjr8717@gmail.com>
2021-02-07 17:21:37 +0900

    2021-02-07

--- 프로그래머스/2021kakao/카드 짝 맞추기.py
+++ 프로그래머스/2021kakao/카드 짝 맞추기.py
@@ -1,34 +1,47 @@
-from collections import defaultdict
 from itertools import permutations
+from collections import defaultdict
 
 
-def getMove(point1, point2): return sum(map(lambda x: 0 if x[1] - x[0] == 0 else 1, zip(point1, point2)))
+def getMove(points):
+    count = 0
+    for i in range(len(points) - 1):
+        piv1 = (points[i][0], points[i + 1][1])  # 좌우 선 이동 시 중간점
+        piv2 = (points[i + 1][0], points[i][1])  # 상하 선 이동 시 중간점
+        route1 = [[(points[i][0], r) for r in (range(points[i][1] + 1, piv1[1] + 1) if piv1[1] > points[i][1] else range(points[i][1] - 1, piv1[1] - 1, -1))],
+                  [(r, points[i + 1][1]) for r in (range(piv1[0] + 1, points[i + 1][0] + 1) if points[i + 1][0] > piv1[0] else range(piv1[0] - 1, points[i + 1][0] - 1, -1))]]
+        route2 = [[(r, points[i][1]) for r in (range(points[i][0] + 1, piv2[0] + 1) if piv2[0] > points[i][0] else range(points[i][0] - 1, piv2[0] - 1, -1))],
+                  [(points[i + 1][0], r) for r in (range(piv2[1] + 1, points[i + 1][1] + 1) if points[i + 1][1] > piv2[1] else range(piv2[1] - 1, points[i + 1][1] - 1, -1))]]
+        r1sum1 = len(route1[0]) if sum(map(lambda x: x in points[i + 1:] or x[1] in (0, 3), route1[0])) == 0 else sum(map(lambda x: x in points[i + 1:] or x[1] in (0, 3), route1[0]))
+        r1sum2 = len(route1[1]) if sum(map(lambda x: x in points[i + 1:] or x[0] in (0, 3), route1[1])) == 0 else sum(map(lambda x: x in points[i + 1:] or x[0] in (0, 3), route1[1]))
+        r2sum1 = len(route2[0]) if sum(map(lambda x: x in points[i + 1:] or x[0] in (0, 3), route2[0])) == 0 else sum(map(lambda x: x in points[i + 1:] or x[0] in (0, 3), route2[0]))
+        r2sum2 = len(route2[1]) if sum(map(lambda x: x in points[i + 1:] or x[1] in (0, 3), route2[1])) == 0 else sum(map(lambda x: x in points[i + 1:] or x[1] in (0, 3), route2[1]))
+        count += min(r1sum1 + r1sum2, r2sum1 + r2sum2)
+    return count
 
 
-def getCount(rc, order, c, count):
+def getPoint(order, move, total):  # 이동 좌표 나열
     if not order:
-        count.append(c)
+        total.append(move)
         return
     cur = order[0]
-    getCount(cur[1], order[1:], c + getMove(rc, cur[0]), count)
-    getCount(cur[0], order[1:], c + getMove(rc, cur[1]), count)
+    getPoint(order[1:], move + [cur[0], cur[1]], total)
+    getPoint(order[1:], move + [cur[1], cur[0]], total)
 
 
 def solution(board, r, c):
-    answer = 0
-    pair = defaultdict(list)
+    answer = list()
+    cards = defaultdict(list)
     for i in range(len(board)):
         for j in range(len(board)):
-            pair[board[i][j]].append((i, j)) if board[i][j] != 0 else None
-    for p in pair.values():
-        answer += getMove(p[0], p[1]) + 2
-    total = list()
-    order = tuple(permutations(pair, len(pair)))
-    for o in order:
-        check_position, count = (r, c), list()
-        getCount(check_position, list(map(lambda x: pair[x], o)), 0, count)
-        total.append(min(count))
-    return answer + min(total)
+            cards[board[i][j]].append((i, j)) if board[i][j] != 0 else None
+    orders = list(permutations(cards, len(cards)))
+    for order in orders:
+        total, count = list(), list()
+        getPoint(list(map(lambda x: cards[x], order)), [(r, c)], total)
+        for t in total:
+            count.append(getMove(t))
+        answer.append(min(count))
+    return min(answer) + 2 * len(cards)
 
 
 _b = [[1, 0, 0, 3], [2, 0, 0, 0], [0, 0, 0, 2], [3, 0, 1, 0]]
