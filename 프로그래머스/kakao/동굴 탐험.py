49f885fae8c3f1081f03f9f5106dc96ade0cca68
wnstjr <wnstjr8717@gmail.com>
2021-03-17 11:41:48 +0900

    2021-03-17

--- 프로그래머스/kakao/동굴 탐험.py
+++ 프로그래머스/kakao/동굴 탐험.py
@@ -5,18 +5,16 @@ class Tree:
             self.path[i[0]].append(i[1])
             self.path[i[1]].append(i[0])
         self.visited = [False for i in range(n)]
-        self.route = [None for i in range(n)]
-        self.pre = dict()
-        self.next = [0]
-        for i in order:
-            self.pre[i[1]] = i[0]
+        self.pre = {i[1]: i[0] for i in order}
+        self.post = dict()
 
     def dfs(self, v):
-        if v in self.pre:
-            if not self.visited[self.pre[v]]:
-                self.next.append(v)
-                return
+        if v in self.pre and not self.visited[self.pre[v]]:
+            self.post[self.pre[v]] = v
+            return
         self.visited[v] = True
+        if v in self.post:
+            self.dfs(self.post[v])
         for i in self.path[v]:
             if not self.visited[i]:
                 self.dfs(i)
@@ -24,28 +22,21 @@ class Tree:
 
 def solution(n, path, order):
     tree = Tree(n, path, order)
-    cycle = 0
-    c = 0
-    while tree.next:
-        tree.dfs(tree.next.pop(0))
-        print(tree.next)
-        if cycle == len(tree.next):
-            c += 1
-            if c == cycle:
-                return False
-        else:
-            cycle = len(tree.next)
-            c = 0
-    return True
+    tree.dfs(0)
+    if sum(tree.visited) == n:
+        return True
+    else:
+        return False
 
-_n = 9
-_path = [[0,1],[0,3],[0,7],[8,1],[3,6],[1,2],[4,7],[7,5]]
-_order = [[8,5],[6,7],[4,1]]
+
+# _n = 9
+# _path = [[0,1],[0,3],[0,7],[8,1],[3,6],[1,2],[4,7],[7,5]]
+# _order = [[8,5],[6,7],[4,1]]
 # _n = 9
 # _path = [[8,1],[0,1],[1,2],[0,7],[4,7],[0,3],[7,5],[3,6]]
 # _order = [[4,1],[5,2]]
-# _n = 9
-# _path = [[0,1],[0,3],[0,7],[8,1],[3,6],[1,2],[4,7],[7,5]]
-# _order = [[4,1],[8,7],[6,5]]
+_n = 9
+_path = [[0,1],[0,3],[0,7],[8,1],[3,6],[1,2],[4,7],[7,5]]
+_order = [[4,1],[8,7],[6,5]]
 
 print(solution(_n, _path, _order))
