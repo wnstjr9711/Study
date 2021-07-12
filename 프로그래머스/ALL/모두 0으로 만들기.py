7e47a87acd46108e1ce2961af8fabdb7b10139d7
wnstjr <wnstjr8717@gmail.com>
2021-07-12 15:52:39 +0900

    2021-07-12

---
+++ 프로그래머스/ALL/모두 0으로 만들기.py
@@ -0,0 +1,14 @@
+def solution(a, edges):
+    answer = -2
+    return answer
+
+
+_a = [-5,0,2,1,2]
+_edges = [[0,1],[3,4],[2,3],[0,3]]
+# _a = [0,1,0]
+# _edges = [[0,1],[1,2]]
+# result = 9
+# result = -1
+import time
+time.sleep(10)
+print(solution(_a, _edges))
