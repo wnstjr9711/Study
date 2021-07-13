0853e6aada9a38a435f42f30af8d996523eb8719
wnstjr <wnstjr8717@gmail.com>
2021-07-13 12:57:08 +0900

    시간초과

--- 프로그래머스/ALL/모두 0으로 만들기.py
+++ 프로그래머스/ALL/모두 0으로 만들기.py
@@ -1,14 +1,27 @@
 def solution(a, edges):
-    answer = -2
+    if sum(a) != 0:
+        return -1
+    answer = 0
+    cnt = {i: list() for i in range(len(a))}
+    for i in edges:
+        cnt[i[0]].append(i[1])
+        cnt[i[1]].append(i[0])
+    while cnt:
+        order = list(sorted(cnt.items(), key=lambda x: len(x[1])))
+        this, target = order.pop(0)
+        cnt.pop(this)
+        cnt[target[0]].remove(this)
+        if not cnt[target[0]]:
+            cnt.clear()
+        a[target[0]] += a[this]
+        answer += abs(a[this])
+        a[this] = 0
     return answer
 
 
-_a = [-5,0,2,1,2]
-_edges = [[0,1],[3,4],[2,3],[0,3]]
-# _a = [0,1,0]
-# _edges = [[0,1],[1,2]]
+_a = [-5, 0, 2, 1, 2]
+_edges = [[0, 1], [3, 4], [2, 3], [0, 3]]
+# _a = [4, -4]
+# _edges = [[0, 1]]
 # result = 9
-# result = -1
-import time
-time.sleep(10)
 print(solution(_a, _edges))
