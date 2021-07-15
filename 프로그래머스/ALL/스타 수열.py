3796c9f8caf3997dbdefdfc3e237a53f33de54b1
wnstjr <wnstjr8717@gmail.com>
2021-07-15 13:15:16 +0900

    시간초과

---
+++ 프로그래머스/ALL/스타 수열.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+
+def solution(a):
+    indexes = defaultdict(list)
+    counts = defaultdict(int)  # answer
+    stack = {i: None for i in a}
+    for i in range(len(a)):
+        for s in stack:
+            if stack[s] is not None and stack[s] != a[i]:
+                if stack[s] == s:
+                    stack[s] = None
+                    counts[s] += 1
+                elif a[i] == s:
+                    stack[s] = None
+                    counts[a[i]] += 1
+            else:
+                stack[s] = a[i]
+        print(stack)
+        indexes[a[i]].append(i)
+    return (max([0] + list(counts.values()))) * 2
+
+# _a = [0]
+_a = [5,2,3,3,5,3]
+# _a = [0,3,3,0,7,2,0,2,2,0]
+print(solution(_a))
