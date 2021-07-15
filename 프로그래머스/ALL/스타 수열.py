c087b3b8b5b7d2ecf83d1433b7a55faa5d0afe7f
wnstjr <wnstjr8717@gmail.com>
2021-07-15 15:51:18 +0900

    2021-07-15

--- 프로그래머스/ALL/스타 수열.py
+++ 프로그래머스/ALL/스타 수열.py
@@ -3,21 +3,22 @@ from collections import defaultdict
 
 def solution(a):
     counts = defaultdict(int)
-    stack = {i: None for i in a}
+    check = dict()
     for i in range(len(a)):
-        for s in stack:
-            if stack[s] is not None and stack[s] != a[i]:
-                if stack[s] == s:
-                    stack[s] = None
-                    counts[s] += 1
-                elif a[i] == s:
-                    stack[s] = None
-                    counts[a[i]] += 1
-            else:
-                stack[s] = a[i]
+        if a[i] not in check:
+            check[a[i]] = 0
+        if i - check[a[i]] > 0 and a[check[a[i]]] != a[i]:
+            counts[a[i]] += 1
+            check[a[i]] = i + 1
+        else:
+            if i + 1 < len(a) and a[i + 1] != a[i]:
+                counts[a[i]] += 1
+                check[a[i]] = i + 2
     return (max([0] + list(counts.values()))) * 2
+
 
 # _a = [0]
 _a = [5, 2, 3, 3, 5, 3]
 # _a = [0, 3, 3, 0, 7, 2, 0, 2, 2, 0]
+_a = [1,1,1,1,1,1,2,3,2,4]
 print(solution(_a))
